013497f63235a0536ef98cb640d65539268466bf
dvirharazi <44293654+dvirharazi@users.noreply.github.com>
2021-03-04 13:35:56 +0200

    Update BestSell.java

--- BestSell.java
+++ BestSell.java
@@ -1,3 +1,7 @@
+//You are given an array prices for which the ith element is the price of a given stock on day i.
+
+//Find the maximum profit you can achieve. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+
 public class BestSell {
     //prices = [7,1,5,3,6,4]
     //prices = [1,2,3,4,5]
